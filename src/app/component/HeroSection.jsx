ff02d5f32723343d31417c73671cf501da3a462d
Aditya <111190263+adi5689@users.noreply.github.com>
2023-10-31 10:51:28 +0530

    Update HeroSection.jsx

--- src/app/component/HeroSection.jsx
+++ src/app/component/HeroSection.jsx
@@ -33,7 +33,7 @@ const HeroSection = () => {
                 1000, // wait 1s before replacing "Mice" with "Hamsters"
                 "A Frontend Developer!",
                 1000,
-                "A UI Designer!",
+                "A Web Designer!",
                 1000,
               ]}
               wrapper="span"
